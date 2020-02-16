e2cb8463708810e3bfbb0fd807a0acbcf711ea83
tasha tran <tashatran@tashas-MacBook-Air.local>
2020-02-16 17:38:16 -0600

    uploading files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("juicy lucy", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("gluten free", FALSE);
